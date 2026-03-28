7dea17c5b274e0d09af6baaafa9b1c80112b1d8f
devendra0123 <devendra13.dc@gmail.com>
2026-03-28 16:53:58 +0545

    Expanding features

--- lib/bot.ts
+++ lib/bot.ts
@@ -11,8 +11,14 @@ export const bot = new Chat({
 });
 
 bot.onNewMention(async (thread, message) => {
-  await thread.post("Hello from Chat SDK test demo by Devendra.");
+  console.log(message);
+  if (message.text === "hi") {
+    await thread.post("Hi Devendra, Good luck for this project");
+  } else {
+    await thread.post("Hello from Chat SDK test demo by Devendra. How can I help you?");
+  }
 });
+
 
 bot.onSubscribedMessage(async (thread, message) => {
   await thread.post(`Devendra said: ${message.text}`);
